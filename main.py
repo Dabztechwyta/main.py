50453c09fcbae2f05bb92a333b18407c750c8690
Victor Dabz <101186335+Dabztechwyta@users.noreply.github.com>
2022-05-28 21:13:54 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# Check if a word is an anagrams 
+# Example:
+# find_anagrams("hello") --> False
+# find_anagrams("racecar") --> True
+
+from distutils.command.check import SilentReporter
+
+
+s1 = input("enter your first word: ")
+s2 = input("enter your second word: ")
+
+def find_anagrams(word):
+    # [assignment] Add your code here
+    if sorted(s1) == sorted(s2):
+        print("this is an anagram")
+        return True
+    else:
+        print("this is not an anagram")
+        return False
+    
+print(find_anagrams(s1))
+
